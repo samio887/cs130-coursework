4b70d508b047465b9318dd823a61fc1bc4fd9ae8
Samantha Olson <samanthaolson2024@u.northwestern.edu>
2021-04-30 16:46:47 -0500

    Adding my tutorial05 to the repo

---
+++ tutorial05/theme-switcher/js/index.js
@@ -0,0 +1,17 @@
+const defaultTheme = () => {
+   document.querySelector(".container").className ="container default";
+};
+
+const oceanTheme = () => {
+   document.querySelector(".container").className ="container ocean";
+};
+
+const desertTheme = () => {
+   document.querySelector(".container").className ="container desert";
+};
+
+
+document.querySelector("#default").onclick = defaultTheme;
+document.querySelector("#ocean").onclick = oceanTheme;
+document.querySelector("#desert").onclick = desertTheme;
+

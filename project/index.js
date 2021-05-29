a04b80123cc5d1328ca83d1dd55095b451f84ba5
Samantha Olson <samanthaolson2024@u.northwestern.edu>
2021-05-28 19:08:09 -0500

    deliverable two

--- project/index.js
+++ project/index.js
@@ -1,108 +1,66 @@
-const images = [
-    'images/field1.jpg',
-    'images/purple.jpg',
-    'images/jar.jpg',
-    'images/green.jpg',
-    'images/green1.jpg',
-    'images/purple1.jpg',
-    'images/magnolias.jpg',
-    'images/daisy1.jpg'
-];
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
 
-const initScreen = () => {
-    images.forEach((image, idx) => {
-        document.querySelector('.cards').innerHTML += `
-            <li class="card">
-                <div class="image" 
-                    style="background-image:url('${image}')"
-                    data-index="${idx}"></div>
-            </li>`;
-    });
-};
-
-
-//initScreen();
-
-
-// const showImage = (ev) => {
-//     const elem = ev.currentTarget;
-//     console.log(elem.style.backgroundImage);
-//     document.querySelector('.featured_image').style.backgroundImage = elem.style.backgroundImage;
-
-// };
-
-
-
-
-       
-// const imageElements = document.querySelectorAll('.image');
-// let currentIndex = 0;
-// for (const elem of imageElements) {
-//     elem.onclick = showImage;
-//     currentIndex += 1;
-
-// }
-
-    
-//Hint 2: currentIndex as a global variable:
-let currentIndex = 0;
-
-// create event handler:
-const showImage = (ev) => {
-    const elem = ev.currentTarget;
-    currentIndex = parseInt(elem.dataset.index);
-    console.log("currentIndex:", currentIndex);
-    displayImage();
-    
-};
-
-const showNext = () => {
-    currentIndex += 1;
-    if (currentIndex >= images.length) {
-        currentIndex = 0;
-
-    }
-    console.log("currentIndex:", currentIndex);
-    // update .featured_image
-    displayImage();
-    
-};
-
-const showPrev = () => {
-    currentIndex -= 1;
-    if (currentIndex < 0) {
-        currentIndex = images.length - 1;
-
-    }
-    console.log("currentIndex:", currentIndex);
-    // update .featured_image
-    displayImage();
-    
-};
-
-const displayImage = () => {
-    const currim = images[currentIndex];
-    document.querySelector('.featured_image').style.backgroundImage = `url('${currim}')`;
-
-    
-
-
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
 }
-// attach event handler to all of the image tags 
-// (after initScreen() has been invoked).
-const attachEventHandlers =()=> {
-    const imageElements = document.querySelectorAll('.image');
-    for (const elem of imageElements) {
-        elem.onclick = showImage;
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function() {
+
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
     }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
+});
+
+$('.main_nav li a').click(function() {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
+});
 
 
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
 
-    document.querySelector('.next').onclick = showNext;
-    document.querySelector('.prev').onclick = showPrev;
-    document.querySelector('.featured_image').onclick = showNext;
-};   
+jQuery(document).ready(function($) {
+
+   $('.smoothscroll').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 800, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+  
+});
 
 
-initScreen();
-attachEventHandlers();
+TweenMax.staggerFrom(".heading", 0.8, {opacity: 0, y: 20, delay: 0.2}, 0.4);
